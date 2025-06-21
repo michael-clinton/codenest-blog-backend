8f9bbd625bf19a64aba8be4835274c4ccb74035b
michael-clinton <michaelclinton084@gmail.com>
2025-06-21 19:54:35 +0530

    fix: update CORS settings to allow Vercel frontend

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://codenest-blog-frontend.vercel.app"],
   credentials: true,
 }));
+
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
